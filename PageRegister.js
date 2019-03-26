a367438dab324a928ad74ec6991b8fcffba8b1dc
Rockyymj <37495081+Rockyymj@users.noreply.github.com>
2019-03-26 21:45:09 +0800

    Add files via upload

---
+++ PageRegister.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// Register page main content
+const PageRegister = () => (
+  <React.Fragment>
+    <h1 className="text-center mb-4">Register</h1>
+
+    <div className="card mx-auto" style={{maxWidth: 500}}>
+      <div className="card-body">
+        <form>
+          <div className="form-group">
+            <input type="email" className="form-control" id="registerEmail" aria-describedby="emailHelp" placeholder="Email address"/>
+          </div>
+          <div className="form-group">
+            <input type="tel" className="form-control" id="registerPhone" aria-describedby="emailHelp" placeholder="Phone number"/>
+          </div>
+
+          <div className="form-group">
+            <input type="password" className="form-control" id="registerPassword1" placeholder="Password"/>
+          </div>
+
+          <div className="form-group">
+            <input type="password" className="form-control" id="registerPassword2" placeholder="Confirm Password"/>
+          </div>
+
+          {/* Submit button */}
+          <button type="submit" className="btn btn-primary mt-3">Submit</button>
+        </form>
+      </div>
+    </div>
+  </React.Fragment>
+);
+// This page is getting information from users and each lines will give report when users make mistakes.
+export default PageRegister;
